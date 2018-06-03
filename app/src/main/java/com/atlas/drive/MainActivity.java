550e48495378a6cd472f6f1f68f9ff48b29b98e9
Patrick Lismore <patricklismore@gmail.com>
2018-06-03 10:54:53 +0100

    Started adding custom login fragments
    Signed-off-by: Patrick Lismore <patricklismore@gmail.com>

--- app/src/main/java/com/atlas/drive/MainActivity.java
+++ app/src/main/java/com/atlas/drive/MainActivity.java
@@ -5,36 +5,98 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.content.Intent;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
 
-public class MainActivity extends AppCompatActivity {
-    private Button mDriver, mCustomer;
+import com.atlas.drive.fragments.LoginFragment;
+import com.atlas.drive.fragments.ResetPasswordDialog;
+
+public class MainActivity extends AppCompatActivity implements ResetPasswordDialog.Listener {
+
+
+    public static final String TAG = MainActivity.class.getSimpleName();
+
+    private LoginFragment mLoginFragment;
+    private ResetPasswordDialog mResetPasswordDialog;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mDriver = (Button) findViewById(R.id.driver);
-        mCustomer = (Button) findViewById(R.id.customer);
+        if (savedInstanceState == null) {
 
-        startService(new Intent(MainActivity.this, onAppKilled.class));
-        mDriver.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, DriverLoginActivity.class);
-                startActivity(intent);
-                finish();
-                return;
-            }
-        });
-
-        mCustomer.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, CustomerLoginActivity.class);
-                startActivity(intent);
-                finish();
-                return;
-            }
-        });
+            loadFragment();
+        }
     }
+
+    private void loadFragment(){
+
+        if (mLoginFragment == null) {
+
+            mLoginFragment = new LoginFragment();
+        }
+        getFragmentManager().beginTransaction().replace(R.id.fragmentFrame,mLoginFragment,LoginFragment.TAG).commit();
+    }
+
+    @Override
+    protected void onNewIntent(Intent intent) {
+        super.onNewIntent(intent);
+
+        String data = intent.getData().getLastPathSegment();
+        Log.d(TAG, "onNewIntent: "+data);
+
+        mResetPasswordDialog = (ResetPasswordDialog) getFragmentManager().findFragmentByTag(ResetPasswordDialog.TAG);
+
+        if (mResetPasswordDialog != null)
+            mResetPasswordDialog.setToken(data);
+    }
+
+    @Override
+    public void onPasswordReset(String message) {
+
+        showSnackBarMessage(message);
+    }
+
+    private void showSnackBarMessage(String message) {
+
+        Snackbar.make(findViewById(R.id.activity_main),message,Snackbar.LENGTH_SHORT).show();
+
+    }
+
+
+
+//    private Button mDriver, mCustomer;
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_main);
+//
+//        mDriver = (Button) findViewById(R.id.driver);
+//        mCustomer = (Button) findViewById(R.id.customer);
+//
+//        startService(new Intent(MainActivity.this, onAppKilled.class));
+//        mDriver.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                Intent intent = new Intent(MainActivity.this, DriverLoginActivity.class);
+//                startActivity(intent);
+//                finish();
+//                return;
+//            }
+//        });
+//
+//        mCustomer.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                Intent intent = new Intent(MainActivity.this, CustomerLoginActivity.class);
+//                startActivity(intent);
+//                finish();
+//                return;
+//            }
+//        });
+//    }
 }
